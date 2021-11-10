66a4757da3f579496de20f9907ea0ea78df2ac3b
Rick Alves <rick_07@hotmail.com.br>
2021-11-10 16:32:23 -0300

    add constants

---
+++ src/app/shared/utils/constants.ts
@@ -0,0 +1,25 @@
+export const emptyString = '';
+
+export const donationPackagesStatus = {
+	Available: 'available',
+	Updated: 'updated',
+	WaitingTransporter: 'waiting transporter',
+	InTransport:'in transport',
+	Delivered: 'delivered',
+	Received: 'received',
+	Canceled: 'canceled',
+}
+
+export const shipmentsStatus = {
+	WaitingForPickup:'waiting for pickup',
+	OnDeliveryRoute: 'on delivery route',
+	Delivered: 'delivered',
+	Canceled: 'canceled',
+	Returned: 'returned',
+}
+
+export const profilesTypes = {
+	Donor: 'donor',
+	Transporter: 'transporter',
+	Distributor: 'distributor',
+}
