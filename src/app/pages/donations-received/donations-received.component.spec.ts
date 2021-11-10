7a6e85eede6b68544ce973aa2893b8e9f262e3a8
Rick Alves <rick_07@hotmail.com.br>
2021-11-10 17:54:52 -0300

    add  donations received page

---
+++ src/app/pages/donations-received/donations-received.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DonationsReceivedComponent } from './donations-received.component';
+
+describe('DonationsReceivedComponent', () => {
+  let component: DonationsReceivedComponent;
+  let fixture: ComponentFixture<DonationsReceivedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DonationsReceivedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DonationsReceivedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/donations-received/donations-received.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donations-received',
+  templateUrl: './donations-received.component.html',
+  styleUrls: ['./donations-received.component.scss']
+})
+export class DonationsReceivedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
