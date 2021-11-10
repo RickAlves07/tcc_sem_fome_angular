1b095c8d7707c65b3a96e81ad4caddfc17b5ad80
Rick Alves <rick_07@hotmail.com.br>
2021-11-10 16:27:23 -0300

    add sign up

---
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-sign-up',
+	templateUrl: './sign-up.component.html',
+	styleUrls: ['./sign-up.component.scss']
+})
+
+@Injectable()
+export class SignUpComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}
