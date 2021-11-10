dd2ce07cbcb0e6520529dca5f37b77e4694d119f
Rick Alves <rick_07@hotmail.com.br>
2021-11-10 17:57:36 -0300

    move pagination component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { HomeComponent } from './pages/home/home.component';
-import { PaginationComponent } from './shared/pagination/pagination.component';
+import { PaginationComponent } from './shared/components/pagination/pagination.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NewDonationComponent } from './pages/new-donation/new-donation.component';
+import { DonationsHistoryComponent } from './pages/donations-history/donations-history.component';
+import { ShipmentsHistoryComponent } from './pages/shipments-history/shipments-history.component';
+import { AvailableDonationsComponent } from './pages/available-donations/available-donations.component';
+import { ScheduledDonationsComponent } from './pages/scheduled-donations/scheduled-donations.component';
+import { DonationsReceivedComponent } from './pages/donations-received/donations-received.component';
+import { TransferDonationComponent } from './pages/transfer-donation/transfer-donation.component';
+import { ListDonationsComponent } from './shared/components/list-donations/list-donations.component';
+import { ListProvisionsComponent } from './shared/components/list-provisions/list-provisions.component';
+import { ListShipmentsComponent } from './shared/components/list-shipments/list-shipments.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
 	declarations: [
@@ -14,7 +26,19 @@ import { HttpClientModule } from '@angular/common/http';
 		LoginComponent,
 		SignUpComponent,
 		HomeComponent,
-		PaginationComponent
+		PaginationComponent,
+  NewDonationComponent,
+  DonationsHistoryComponent,
+  ShipmentsHistoryComponent,
+  AvailableDonationsComponent,
+  ScheduledDonationsComponent,
+  DonationsReceivedComponent,
+  TransferDonationComponent,
+  ListDonationsComponent,
+  ListProvisionsComponent,
+  ListShipmentsComponent,
+  HeaderComponent,
+  FooterComponent
 	],
 	imports: [
 		BrowserModule,
