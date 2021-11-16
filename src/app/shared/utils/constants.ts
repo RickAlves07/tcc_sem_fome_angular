da4429ad084d91a88b7bc1d1486fc71b6c5c50f8
Rick Alves <rick_07@hotmail.com.br>
2021-11-16 00:57:29 -0300

    refactor donations status and data formats

--- src/app/shared/utils/constants.ts
+++ src/app/shared/utils/constants.ts
@@ -1,21 +1,23 @@
 export const emptyString = '';
 
+export const DateFormats = {
+	DateOnlyFormatPtBr: "DD/MM/YYYY",
+	DateOnlyDefaultFormat: "YYYY-MM-DD",
+	DateTimeOnlyFormatPtBr: "DD/MM/YYYY HH:mm",
+	DateTimeDefaultFormat: "YYYY-MM-DDTHH:mm:ss",
+	DateTimeZoneISO: 'YYYY-MM-DDTHH:mm:ss.sssZ'
+}
+
 export const donationPackagesStatus = {
-	Available: 'available',
-	Updated: 'updated',
-	WaitingTransporter: 'waiting transporter',
+	WaitingATransporter: 'waiting a transporter',
+	WaitingForPickup:'waiting for pickup',
+	OnDeliveryRoute: 'on delivery route',
 	InTransport:'in transport',
 	Delivered: 'delivered',
 	Received: 'received',
-	Canceled: 'canceled',
-}
-
-export const shipmentsStatus = {
-	WaitingForPickup:'waiting for pickup',
-	OnDeliveryRoute: 'on delivery route',
-	Delivered: 'delivered',
-	Canceled: 'canceled',
+	ReturningToDonor: 'returning to donor',
 	Returned: 'returned',
+	Canceled: 'canceled',
 }
 
 export const profilesTypes = {
