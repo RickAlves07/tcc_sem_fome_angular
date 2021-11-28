084ea5ab5a51170c85a3ec78dadb3ccf20d8664c
Rick Alves <rick_07@hotmail.com.br>
2021-11-28 14:22:00 -0300

    add middlewares

---
+++ src/app/middleware/jwt-interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication/authentication.service';
+
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+	constructor(private authenticationService: AuthenticationService) { }
+
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		let currentUser: any = this.authenticationService.checkLoggedIn();
+
+		if (currentUser && currentUser.data.token) {
+			console.log('JwtInterceptor');
+			request = request.clone({
+				setHeaders: {
+					Authorization: `Bearer ${currentUser.data.token}`,
+				}
+			});
+		}
+		console.log(request);
+		return next.handle(request);
+	}
+}
